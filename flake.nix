81579306fbe3bf23ec3c0e977ab4148bcfb9cd2b
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-08-06 15:33:47 +0200

    chore: flake only for dev tools

--- flake.nix
+++ flake.nix
@@ -26,44 +26,12 @@
       let
         # Nightly rust used for wasm runtime compilation
         rust-nightly = rust-bin.nightly.latest.default;
-
-        # Crane lib instantiated with current nixpkgs
-        crane-lib = crane.mkLib pkgs;
-
-        # Crane pinned to nightly Rust
-        crane-nightly = crane-lib.overrideToolchain rust-nightly;
-
-        # Default args to crane
-        common-args = {
-          src = lib.cleanSourceWith {
-            filter = lib.cleanSourceFilter;
-            src = lib.cleanSourceWith {
-              filter = nix-gitignore.gitignoreFilterPure (name: type: true)
-                [ ./.gitignore ] ./.;
-              src = ./.;
-            };
-          };
-        };
-
-        # Common dependencies used for caching
-        common-deps = crane-nightly.buildDepsOnly common-args;
-
-        common-cached-args = common-args // { cargoArtifacts = common-deps; };
-
       in rec {
-        packages.pkg = crane-nightly.buildPackage common-cached-args;
-        packages.default = packages.pkg;
-        checks = {
-          package = packages.default;
-          clippy = crane-nightly.cargoClippy (common-cached-args // {
-            cargoClippyExtraArgs = "-- --deny warnings";
-          });
-          fmt = crane-nightly.cargoFmt common-args;
-        };
         devShell = mkShell {
-          buildInputs = [ rust-nightly clang protobuf openssl pkg-config];
+          buildInputs = [ rust-nightly clang protobuf openssl pkg-config  ];
           LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-          LD_LIBRARY_PATH = lib.makeLibraryPath [ stdenv.cc.cc.lib openssl protobuf ];
+          LD_LIBRARY_PATH =
+            lib.makeLibraryPath [ clangStdenv.cc.cc.lib openssl protobuf ];
           CPLUS_INCLUDE_PATH = "${openssl.dev}/include";
         };
       });
